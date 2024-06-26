3fee0cc82561816bd5c46f0f357cddba1bfdd81c
reokdev <reokwebdev@gmail.com>
2024-06-26 10:18:58 +0000

    initial setup

---
+++ oli-harris-portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import Nav from './components/Nav'
+import Header from './components/Header'
+import Grid from './components/Grid'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Nav />
+    <Header />
+    <Grid />
+    <Contact />
+    <Footer />
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
